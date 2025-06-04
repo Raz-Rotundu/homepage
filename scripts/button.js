cb4e98556bc9f854665dae88615449522a8d5709
Razvan Rotundu <raz.rotundu@gmail.com>
2025-06-04 15:57:18 -0400

    add home button

---
+++ scripts/button.js
@@ -0,0 +1,24 @@
+// Controls the home button, having it follow page on scroll
+
+
+document.addEventListener("DomContentLoaded", function(){
+    let homeButton = document.getElementById("homeButton");
+
+    // Call scrollfunction on scroll
+    window.onscroll = function() {scrollFunction()};
+});
+
+// Function to follow button down
+function scrollFunction() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        homeButton.style.display = "block";    
+    } else {
+        homeButton.style.display = "none";
+    }
+}
+
+// Function to send user back to top
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
