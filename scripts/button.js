18e37b6d24534ab774765beb0b4f53d26c3ae968
Razvan Rotundu <raz.rotundu@gmail.com>
2025-06-04 16:13:47 -0400

    fix button bugs

--- scripts/button.js
+++ scripts/button.js
@@ -1,24 +1,26 @@
 // Controls the home button, having it follow page on scroll
 
 
-document.addEventListener("DomContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function(){
     let homeButton = document.getElementById("homeButton");
 
     // Call scrollfunction on scroll
     window.onscroll = function() {scrollFunction()};
+
+
+        // Function to follow button down
+    function scrollFunction() {
+        if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            homeButton.style.display = "block";    
+        } else {
+            homeButton.style.display = "none";
+        }
+    }
+
+    // Function to send user back to top
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
 });
 
-// Function to follow button down
-function scrollFunction() {
-    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        homeButton.style.display = "block";    
-    } else {
-        homeButton.style.display = "none";
-    }
-}
-
-// Function to send user back to top
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
