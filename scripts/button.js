8307c2b7e0ae72f27aa12e67dae1d0585da2164f
Razvan Rotundu <raz.rotundu@gmail.com>
2025-06-05 11:36:17 -0400

    add JS to switch header styling on mobile

--- scripts/button.js
+++ scripts/button.js
@@ -8,7 +8,28 @@ document.addEventListener("DOMContentLoaded", function(){
     window.onscroll = function() {scrollFunction()};
 
 
-        // Function to follow button down
+
+    // Detect if mobile device or not
+    /* Storing user's device details in a variable*/
+    let details = navigator.userAgent;
+
+    /* Creating a regular expression 
+    containing some mobile devices keywords 
+    to search it in details string*/
+    let regexp = /android|iphone|kindle|ipad/i;
+
+    /* Using test() method to search regexp in details
+    it returns boolean value*/
+    let isMobileDevice = regexp.test(details);
+
+    if (isMobileDevice) {
+        let headerLinks = document.getElementById("links");
+        headerLinks.style.flexDirection = "column";
+
+        console.log("You are using a Mobile Device");
+    }
+
+    // Function to follow button down
     function scrollFunction() {
         if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             homeButton.style.display = "block";    
